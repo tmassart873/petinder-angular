45bb8dede1b9340096af6cfadce1e950416a8992
tmassart873 <tim.massart@hotmail.com>
2022-01-10 10:12:16 +0100

    linked routerModule

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
+import {RouterModule} from "@angular/router";
 
 
 
@@ -13,7 +14,8 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class LayoutModule { }
